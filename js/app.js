8bb652fb9d8c32d73ed903d3e6aad5900516f307
astidhiyaa <astidhiyaanzaria@gmail.com>
2020-10-17 10:52:15 +0700

    int comm

---
+++ js/app.js
@@ -0,0 +1,59 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAHhoX9lCu61pT8Ithq5YhI8g5gjI9E1n4",
+    authDomain: "sofalio.firebaseapp.com",
+    databaseURL: "https://sofalio.firebaseio.com",
+    projectId: "sofalio",
+    storageBucket: "sofalio.appspot.com",
+    messagingSenderId: "731095595011",
+    appId: "1:731095595011:web:f47144df128cad7e38f636",
+    measurementId: "G-3T0SCJL28T"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  function getSofa(){
+      const dbref = firebase.database().ref();
+      var sofasHTML = "";
+        dbref.on("child_added", snapshot =>{
+            let sofa = snapshot.val()
+            sofasHTML += `
+              <div class="card">
+                <a href="./sofa.html?id=${sofa.id}">
+                  <div class="card-image waves-effect waves-block waves-light">
+                    <img src="${sofa.image}" />
+                  </div>
+                </a>
+                <div class="card-content">
+                  <span class="card-title truncate">${sofa.product_name}</span>
+                  <p>${sofa.price}</p>
+                </div>
+              </div>
+            `;
+            // console.log("Nama",sofa.product_name);
+            // console.log("Price",sofa.price);
+            document.getElementById("sofa").innerHTML = sofasHTML;
+    });
+  }
+
+  function getSelectedSofa(){
+    var urlParams = new URLSearchParams(window.location.search);
+    var idParam = urlParams.get("id");
+
+    const dbRef = firebase.database().ref().child(idParam);
+    dbRef.once("value").then(function(snapshot){
+        var sofa = snapshot.val()
+          var sofasHTML = `
+          <div class="card">
+          <div class="card-image waves-effect waves-block waves-light">
+            <img src="${sofa.image}" />
+          </div>
+          <div class="card-content">
+            <span class="card-title">${sofa.product_name}</span>
+            <span class="card-title">${sofa.price}</span>
+            <span class="card-title">${sofa.material}</span>
+            <span class="card-title">${sofa.dimension}</span>
+          </div>
+        </div>
+      `;
+          document.getElementById("body-content").innerHTML = sofasHTML;
+    });
+  }
---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "sofalio";
+
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/nav.html",
+    "/css/materialize.min.css",
+    "/js/materialize.min.js",
+    "/js/app.js",
+    "/pages/sofa.html",
+];
+
